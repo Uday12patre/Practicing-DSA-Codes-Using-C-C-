4cd8172fe5a1c86694c39dbffd66e3b7de7baa12
Uday Barapatre <uday12patre.code@gmail.com>
2025-12-13 18:59:51 +0530

    Implement Tower of Hanoi using recursion

---
+++ TowerOfHanoiRecursion.cpp
@@ -0,0 +1,20 @@
+// Tower Of Hanoi Recursion
+#include<iostream>
+using namespace std;
+
+void TOH(int n, int A, int B, int C)
+{
+    if(n>0)
+    {
+        TOH(n-1,A,C,B);
+        cout << "From " << A << " to " << C << endl;
+        TOH(n-1,B,A,C);
+    }
+}
+
+int main()
+{
+    cout << "Solution for Tower Of Hanoi: " << endl;
+    TOH(3,1,2,3);
+    return 0;
+}
